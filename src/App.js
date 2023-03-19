ca7f4c2041e25809f45bbbec076d6652979b8b01
maaz619 <maazahmedlfc@gmail.com>
2023-03-19 23:49:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+const Box = ({ className = "", size, style, ...props }) => {
+  const sizeClassName = size ? `box--${size}` : "";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      style={{ fontStyle: "italic", ...style }}
+      className={`box ${sizeClassName}`.trim()}
+      {...props}
+    />
   );
-}
+};
+
+const App = () => {
+  return (
+    <>
+      <Box
+        size="small"
+        style={{ backgroundColor: "lightblue" }}
+        children="Hello"
+      />
+      <Box size="medium" style={{ backgroundColor: "pink" }} children="Hello" />
+      <Box
+        size="large"
+        style={{ backgroundColor: "orange" }}
+        children="Hello"
+      />
+      <Box children="sizeless" />
+    </>
+  );
+};
 
 export default App;
